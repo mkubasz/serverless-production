7e4a2c90fa322cf1707ca8d9a98be8ea3fdf8bca
mkubasz <mateusz.kubaszek@lamin.ar>
2023-01-28 00:55:15 +0100

    update ssm

--- functions/get-restaurants.js
+++ functions/get-restaurants.js
@@ -1,9 +1,12 @@
 const { DynamoDBClient, ScanCommand } = require('@aws-sdk/client-dynamodb');
 const { unmarshall } = require('@aws-sdk/util-dynamodb');
+const middy = require('@middy/core');
+const ssm = require('@middy/ssm');
+
 const dynamoDb = new DynamoDBClient({ region: 'us-east-1' });
 
-const defaultResults = process.env.defaultResults || 8;
 const tableName = process.env.restaurants_table;
+const { serviceName, stage } = process.env;
 
 const getRestaurants = async (limit) => {
     console.log(`Querying for ${limit} restaurants`);
@@ -16,11 +19,18 @@ const getRestaurants = async (limit) => {
     return result.Items.map(item => unmarshall(item));
 }
 
-module.exports.handler = async (event, context) => {
-    const restaurants = await getRestaurants(defaultResults);
+module.exports.handler = middy(async (event, context) => {
+    const restaurants = await getRestaurants(context.config.defaultResults);
     const response = {
         statusCode: 200,
         body: JSON.stringify(restaurants)
     };
     return response;
-}
+}).use(ssm({
+    cache: true,
+    cacheExpiryInMillis: 5 * 60 * 1000,
+    setToContext: true,
+    fetchData: {
+        config: `/${serviceName}/${stage}/get-restaurants/config`
+    }
+}));
--- functions/search-restaurants.js
+++ functions/search-restaurants.js
@@ -1,10 +1,12 @@
 const { DynamoDBClient, ScanCommand } = require('@aws-sdk/client-dynamodb');
 const { unmarshall } = require('@aws-sdk/util-dynamodb');
+const middy = require('@middy/core');
+const ssm = require('@middy/ssm');
+
 const dynamoDb = new DynamoDBClient({ region: 'us-east-1' });
 
-const defaultResults = process.env.defaultResults || 8;
 const tableName = process.env.restaurants_table;
-
+const { serviceName, stage } = process.env;
 const findRestaurantsByTheme = async (theme, limit) => {
     console.log(`Querying for ${limit} restaurants with theme ${theme}`);
     const params = {
@@ -20,13 +22,22 @@ const findRestaurantsByTheme = async (theme, limit) => {
     return result.Items.map(item => unmarshall(item));
 }
 
-module.exports.handler = async (event, context) => {
+module.exports.handler = middy(async (event, context) => {
     const req = JSON.parse(event.body);
+    console.info(context.secretString)
     const theme = req.theme;
-    const restaurants = await findRestaurantsByTheme(theme, defaultResults);
+    const restaurants = await findRestaurantsByTheme(theme, context.config.defaultResults);
     return {
         statusCode: 200,
         body: JSON.stringify(restaurants),
         isBase64Encoded: false
     };
-}
+}).use(ssm({
+    cache: true,
+    cacheExpiryInMillis: 5 * 60 * 1000,
+    setToContext: true,
+    fetchData: {
+        config: `/${serviceName}/${stage}/search-restaurants/config`,
+        secretString: `/${serviceName}/${stage}/search-restaurants/secretString`
+    }
+}));
